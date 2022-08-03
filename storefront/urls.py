03e829dd4ebe6ad0501bb362d3db50262660c16a
menilik <spyxmeni@gmail.com>
2022-08-03 13:20:17 +0300

    add customer url

--- storefront/urls.py
+++ storefront/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('playground/', include('playground.urls')),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
